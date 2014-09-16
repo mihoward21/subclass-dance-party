5855136b91c07448e7f5f64b3d11bef5c2f95365
darkstar21 <mikeh09@gmail.com>
2014-09-15 17:49:43 -0700

    Javascript components working, now to work on style

--- spec/bouncyDancerSpec.js
+++ spec/bouncyDancerSpec.js
@@ -1,35 +1,35 @@
-describe("blinkyDancer", function() {
+describe("bouncyDancer", function() {
 
-  var blinkyDancer;
+  var bouncyDancer;
   var timeBetweenSteps = 100;
   var clock;
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = new makeBlinkyDancer(10, 20, timeBetweenSteps);
+    bouncyDancer = new makeBouncyDancer(10, 20, timeBetweenSteps);
   });
 
   it("should have a jQuery $node object", function(){
-    expect(blinkyDancer.$node).to.be.an.instanceof(jQuery);
+    expect(bouncyDancer.$node).to.be.an.instanceof(jQuery);
   });
 
   it("should have a step function that makes its node blink", function() {
-    sinon.spy(blinkyDancer.$node, 'toggle');
-    blinkyDancer.step();
-    expect(blinkyDancer.$node.toggle.called).to.be.true;
+    sinon.spy(bouncyDancer.$node, 'toggle');
+    bouncyDancer.step();
+    expect(bouncyDancer.$node.toggle.called).to.be.true;
   });
 
   describe("dance", function(){
     it("should call step at least once per second", function(){
-      sinon.spy(blinkyDancer, "step");
-      expect(blinkyDancer.step.callCount).to.be.equal(0);
+      sinon.spy(bouncyDancer, "step");
+      expect(bouncyDancer.step.callCount).to.be.equal(0);
       //clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
 
-      expect(blinkyDancer.step.callCount).to.be.equal(1);
+      expect(bouncyDancer.step.callCount).to.be.equal(1);
 
       clock.tick(timeBetweenSteps);
-      expect(blinkyDancer.step.callCount).to.be.equal(2);
+      expect(bouncyDancer.step.callCount).to.be.equal(2);
     });
   });
 
---
+++ src/bouncyDancer.js
@@ -0,0 +1,21 @@
+var makeBouncyDancer = function(top, left, timeBetweenSteps){
+  makeRocker.call(this, top, left, timeBetweenSteps);
+
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+};
+
+makeBouncyDancer.prototype = Object.create(makeRocker.prototype);
+makeBouncyDancer.prototype.constructor = makeBouncyDancer;
+
+makeBouncyDancer.prototype.step = function(timeBetweenSteps){
+  // call the old version of step at the beginning of any call to this new version of step
+  makeRocker.prototype.step.call(this, timeBetweenSteps);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+
+  this.$node.toggle();
+};
+
